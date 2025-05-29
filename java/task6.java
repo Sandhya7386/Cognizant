b1fd8b3abd8b572804c5e9ca652f95da1c82c54f
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 12:11:52 +0530

    Update task6.java

--- java/task6.java
+++ java/task6.java
@@ -1,1 +1,14 @@
-
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int integerValue = 25;        
+        float floatValue = 5.75f;      
+        double doubleValue = 10.99;    
+        char charValue = 'A';          
+        boolean booleanValue = true;   
+        System.out.println("Integer Value: " + integerValue);
+        System.out.println("Float Value: " + floatValue);
+        System.out.println("Double Value: " + doubleValue);
+        System.out.println("Character Value: " + charValue);
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+}
