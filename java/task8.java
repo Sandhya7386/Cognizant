502f382da7e2ecf90a543c5f5a52f1f07d5af22e
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 12:19:53 +0530

    Update task8.java

--- java/task8.java
+++ java/task8.java
@@ -1,1 +1,14 @@
-
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        int result1 = 10 + 5 * 2;
+        System.out.println("Expression 1: 10 + 5 * 2 = " + result1);
+        int result2 = (10 + 5) * 2;
+        System.out.println("Expression 2: (10 + 5) * 2 = " + result2);
+        int result3 = 10 / 2 + 3;
+        System.out.println("Expression 3: 10 / 2 + 3 = " + result3);
+        int result4 = 10 + 5 * 2 - 3;
+        System.out.println("Expression 4: 10 + 5 * 2 - 3 = " + result4);
+        int result5 = 10 + 5 / 5 * 2;
+        System.out.println("Expression 5: 10 + 5 / 5 * 2 = " + result5);
+    }
+}
