6e58f669ab658924d2d832d7f30c996ecdcc6f2f
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 12:00:13 +0530

    Update task4.java

--- java/task4.java
+++ java/task4.java
@@ -1,1 +1,23 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year:");
+        int n = sc.nextInt();
+        if (n % 4 == 0) {
+            if (n % 100 == 0) {
+                if (n % 400 == 0) {
+                    System.out.println("Leap year");
+                } else {
+                    System.out.println("Not a Leap year");
+                }
+            } else {
+                System.out.println("Leap year");
+            }
+        } else {
+            System.out.println("Not a Leap year");
+        }
 
+        sc.close();
+    }
+}
