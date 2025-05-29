d9c9672f8a7646db1ab7161cc83ffa960e793ae8
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 12:16:10 +0530

    Update task7.java

--- java/task7.java
+++ java/task7.java
@@ -1,1 +1,16 @@
+public class TypeCastingDemo {
+    public static void main(String[] args) {
+        double doubleValue = 9.99;
+        System.out.println("Original double value: " + doubleValue);
+
+        int intValue = (int) doubleValue;
+        System.out.println("Double casted to int: " + intValue);
+
+        int intVar = 10;
+        System.out.println("Original int value: " + intVar);
+
+        double doubleVar = (double) intVar;
+        System.out.println("Int casted to double: " + doubleVar);
+    }
+}
 
