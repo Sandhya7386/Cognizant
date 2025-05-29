4ae3cf45d1fa14120d65fe868e94bce9c158a08e
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 12:08:52 +0530

    Update task5.java

--- java/task5.java
+++ java/task5.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+           int result=n*i;
+        
+        System.out.println(n+"*"+i+"="+result);
+        }
+    }
+}
