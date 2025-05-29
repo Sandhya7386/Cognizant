c1ad8adb24b3a1b357dc45088be1784e675c2cd4
Sandhya7386 <sandhyapottipati5@gmail.com>
2025-05-29 13:10:26 +0530

    Update task15.java

--- java/task15.java
+++ java/task15.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class StringReversal {
+
+    public static void main(String[] args) {
+        // Create a scanner object to get user input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user for a string
+        System.out.print("Enter a string: ");
+        String inputString = sc.nextLine();
+
+        // Use StringBuilder to reverse the string
+        StringBuilder reversedString = new StringBuilder(inputString);
+        reversedString.reverse();
+
+        // Display the reversed string
+        System.out.println("Reversed string: " + reversedString.toString());
+
+        // Close the scanner
+        sc.close();
+    }
+}
 
